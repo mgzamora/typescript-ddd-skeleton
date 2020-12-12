47d2be375f0d1a1222841e651bc3ebd3fc290a83
Manuel Gutiérrez <mgzamora@gmail.com>
2020-12-12 19:06:15 +0000

    added get courses handler application uc

--- src/Contexts/Mooc/Courses/application/GetCourses/CoursesResponse.ts
+++ src/Contexts/Mooc/Courses/application/GetCourses/CoursesResponse.ts
@@ -1,13 +1,16 @@
 import { Course } from '../../domain/Course';
 import { CourseResponse } from '../../../Shared/domain/Courses/application/CourseResponse';
+import { Nullable } from '../../../../Shared/domain/Nullable';
 export class CoursesResponse {
     readonly data: CourseResponse[];
      
-    constructor(courses: Course[]) {
+    constructor(courses: Nullable<Course[]>) {
         this.data = [];
-        courses.forEach(course => {
-            this.data.push(new CourseResponse(course))
-        });        
+        if (courses !== null) {
+            courses.forEach(course => {
+                this.data.push(new CourseResponse(course))
+            }); 
+        }       
     }
 }
   
