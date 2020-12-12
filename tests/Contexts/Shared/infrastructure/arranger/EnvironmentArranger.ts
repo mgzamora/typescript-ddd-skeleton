73b64f5a2d6f32922f362e7ed11c86730862d093
Manuel Gutiérrez <mgzamora@gmail.com>
2020-12-12 18:53:37 +0000

    added add course to environment arranger

--- tests/Contexts/Shared/infrastructure/arranger/EnvironmentArranger.ts
+++ tests/Contexts/Shared/infrastructure/arranger/EnvironmentArranger.ts
@@ -2,4 +2,6 @@ export abstract class EnvironmentArranger {
   public abstract arrange(): Promise<void>;
 
   public abstract close(): Promise<void>;
+
+  public abstract addCourse(): Promise<void>;
 }
